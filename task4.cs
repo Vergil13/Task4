0206349a70af7e2c679c092c0176a57c7c2d1354
Вергилий Двачевский <Vergils2ch@gmail.com>
2022-09-07 22:18:49 +0300

    сделал новую задачку 4 (по счету вторую)

---
+++ task4.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int x=Convert.ToInt32(Console.ReadLine ( ) );
+Console.WriteLine("Введите число");
+int y=Convert.ToInt32(Console.ReadLine ( ) ); 
+Console.WriteLine("Введите число");
+int z=Convert.ToInt32(Console.ReadLine ( ) ); 
+int max=x;
+if (y>max) max =y;
+if (z>max) max=z;
+else Console.WriteLine(max);
+
